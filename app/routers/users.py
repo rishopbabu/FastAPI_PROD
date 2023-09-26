f9e62c944fb4b827728176b5e0a52230066a511e
Rishop Babu <rishopbabu22@gmail.com>
2023-09-26 10:46:18 +0530

    user response changed

--- app/routers/users.py
+++ app/routers/users.py
@@ -16,12 +16,52 @@ router = APIRouter(
 
 
 # Create a new users account or Rejister a new user
+# @router.post(
+#     "/register",
+#     status_code=status.HTTP_201_CREATED,
+#     response_model=schemas.CreateUserResponse,
+# )
+# def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
+#     # hash the user password - user.password
+#     hashed_pwd = utils.hash_password(
+#         user.password
+#     )  # getting the password from user input and store in a variable
+#     user.password = hashed_pwd  # set the user.password a hashed password
+
+#     # create a new user
+#     new_user = models.Users(**user.model_dump())
+#     db.add(new_user)
+#     db.commit()
+#     db.refresh(new_user)
+
+#     return new_user
+
 @router.post(
     "/register",
     status_code=status.HTTP_201_CREATED,
     response_model=schemas.CreateUserResponse,
 )
 def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
+    # check if the registered user already exist or not
+    existing_user_email = db.query(models.Users).filter(models.Users.email == user.email).first()
+    existing_user_phone = db.query(models.Users).filter(models.Users.phone == user.phone).first()
+
+    if existing_user_email and existing_user_phone:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f'User with email: {user.email} and phone number: {user.phone} already exists',
+        )
+    elif existing_user_email:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f'User with email: {user.email} already exists',
+        )
+    elif existing_user_phone:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f'User with phone number: {user.phone} already exists'
+        )
+
     # hash the user password - user.password
     hashed_pwd = utils.hash_password(
         user.password
